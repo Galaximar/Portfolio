7e8f39a774d0c8c43f6bfa5acb407e1db34281de
Marcelo Gottardini <89224636+Galaximar@users.noreply.github.com>
2022-04-15 23:23:51 -0300

    projec

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -8,10 +8,11 @@ import { useEffect, useRef, useState } from "react";
 import Menu from "./Menu";
 import everyOne from "../assets/everyOne.jpg"
 
-const foodApi={video:"https://www.youtube.com/watch?v=ykrehetseuc",title:"Food Api",date:"January '1",image:"https://apilist.fun/images/social/api/food-api.png",description:"FoodApi utilizando..",role:"FullStack Developer.",tecnologies:{Frontend:["React","Redux","Css"],Backend:["Express","Sequelieze","Node",],Database:["PostgreSQL"]}}
-const everyoneStore={deploy:"https://respaldo-everyones-store.vercel.app/",title:"Everyone´s Store",date:"March '1",image:everyOne,description:"Eccomerce utilizando..",role:"FullStack Developer.",tecnologies:{Frontend:["React","Redux","Css"],Backend:["Express","Sequelieze","Node",],Database:["PostgreSQL"]}}
+const foodApi={video:"https://www.youtube.com/watch?v=ykrehetseuc",title:"Food Api",date:"January '1",image:"https://apilist.fun/images/social/api/food-api.png",description:"FoodApi utilizando..",role:"FullStack Developer.",tecnologies:{Frontend:["React","Redux","Css"],Backend:["Express","Sequelieze","Node",],Database:["PostgreSQL"]}};
+const everyoneStore={deploy:"https://respaldo-everyones-store.vercel.app/",title:"Everyone´s Store",date:"March '1",image:everyOne,description:"Eccomerce utilizando..",role:"FullStack Developer.",tecnologies:{Frontend:["React","Redux","Css"],Backend:["Express","Sequelieze","Node",],Database:["PostgreSQL"]}};
+const blackJack={deploy:"https://blackjackapp.vercel.app",title:"Blackjack",date:"July '1-2021",image:"https://juegos.arkadiumarena.com/arenaxstorage-blob/arenax-games/BlackJack/0.51/build/resources/assets/thumbs/secondary-thumb2x.jpg",description:"A simple Blackjack game.",role:"Frontend Developer. One of the first projects to develop",tecnologies:{Frontend:["Javascript","Uderscore","Css","Bootstrap"]}};
 const data=[
-  everyoneStore,foodApi,foodApi,foodApi
+  everyoneStore,foodApi,foodApi,blackJack
 ]
 export default function Body() {
   const aboutRef=useRef(null);
