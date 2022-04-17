6272bb50672f294216ce69fd06f2a89b7276bf8b
Marcelo Gottardini <89224636+Galaximar@users.noreply.github.com>
2022-04-17 04:34:45 -0300

    project

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -14,9 +14,10 @@ It also consumes data provided by a food API by displaying<br/> it
 with their respective filters and pagination.</p>,role:"FullStack Developer.",tecnologies:{Frontend:["React","Redux","Css"],Backend:["Express","Sequelieze","Node",],Database:["PostgreSQL"]}};
 const everyoneStore={deploy:"https://respaldo-everyones-store.vercel.app/",title:"Everyone´s Store",date:"March '1",image:everyOne,description:<p>Ecommerce of technological products, using Paypal as a payment gateway.<br/>The user can: Add a product to a shopping cart, make<br/> a purchase, add a product to favorites, make a review, interact<br/>with a chat bot, see the branches on a map and subscribe<br/>to notifications about news of the pages.<br/>
 It also has an administrative panel, in which you can:<br/>Create/edit/delete a category, product,<br/>ban a user, convert user to admin, view transactions.</p>,role:"FullStack Developer.",tecnologies:{Frontend:["React","Redux","Css"],Backend:["Express","Sequelieze","Node",],Database:["PostgreSQL"]}};
+const weather={deploy:"https://weatherappclime.vercel.app/",title:"Weather App",date:"Dec '1-2021",image:"https://static01.nyt.com/images/2014/12/11/technology/personaltech/11machin-illo/11machin-illo-superJumbo-v3.jpg",description:<p>A simple Weather app.</p>,role:"Frontend Developer.",tecnologies:{Frontend:["Javascript","Reac","Css","Bootstrap","Material UI","Local Storage"]}};
 const blackJack={deploy:"https://blackjackapp.vercel.app",title:"Blackjack",date:"July '1-2021",image:"https://juegos.arkadiumarena.com/arenaxstorage-blob/arenax-games/BlackJack/0.51/build/resources/assets/thumbs/secondary-thumb2x.jpg",description:<p>A simple Blackjack game.<br/>One of the first projects to develop</p>,role:"Frontend Developer.",tecnologies:{Frontend:["Javascript","Uderscore","Css","Bootstrap"]}};
 const data=[
-  everyoneStore,foodApi,blackJack
+  everyoneStore,foodApi,weather,blackJack
 ]
 export default function Body() {
   const aboutRef=useRef(null);
